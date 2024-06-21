5d19f8e9025241dd542a1ea08689d81f04fc4728
100ngram <sangramjitdutta@gmail.com>
2024-06-22 00:10:05 +0530

    feat : added signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -40,6 +40,34 @@ app.post("/api/v1/signup", async (c) => {
   }
 });
 
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const jwtResponse = await sign(
+      {
+        user: {
+          email: body.email,
+        },
+      },
+      c.env.JWT_SECRET
+    );
+    return c.json({ token: jwtResponse });
+  } catch (e) {
+    console.log("caught", e);
+    c.status(403);
+    return c.json({ err: e });
+  }
+});
+
 // app.get("/api/v1/blog/:id", (c) => {
 //   const id = c.req.param("id");
 //   console.log(id);
