39884d839c41cc5af45dedeeed0ac22bc9033743
100ngram <sangramjitdutta@gmail.com>
2024-06-30 23:49:45 +0530

    fix: escape character build error

--- frontend/app/_components/edit-dialog.tsx
+++ frontend/app/_components/edit-dialog.tsx
@@ -88,7 +88,7 @@ export function EditDialog({ article, id, session }: any) {
         <DialogHeader>
           <DialogTitle>Edit Blog</DialogTitle>
           <DialogDescription>
-            Make changes to your blog here. Click save when you're done.
+            Make changes to your blog here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <form
