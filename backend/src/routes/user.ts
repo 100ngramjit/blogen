4f74138ffb0a859c527d07ae553f31c07bba0ade
sangramjit dutta <sangramjitdutta@gmail.com>
2024-08-09 13:38:42 +0530

    feat: hashed password stored in db

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,6 +3,7 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { sign } from "hono/jwt";
 import { envVars } from "../types";
+import bcrypt from "bcryptjs";
 
 export const userRouter = new Hono<envVars>();
 
@@ -12,10 +13,13 @@ userRouter.post("/signup", async (c) => {
   }).$extends(withAccelerate());
   try {
     const body = await c.req.json();
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
     const user = await prisma.user.create({
       data: {
         email: body.email,
-        password: body.password,
+        password: hashedPassword,
         name: body.name,
       },
     });
@@ -39,21 +43,26 @@ userRouter.post("/signin", async (c) => {
   }).$extends(withAccelerate());
   const body = await c.req.json();
   try {
-    const user = await prisma.user.findFirst({
+    console.log(body);
+    const user = await prisma.user.findUnique({
       where: {
         email: body.email,
-        password: body.password,
       },
     });
+    console.log(user);
     if (user) {
-      const jwtResponse = await sign(
-        {
-          email: body.email,
-          id: user?.id,
-        },
-        c.env.JWT_SECRET
-      );
-      return c.json({ token: jwtResponse, name: user.name });
+      // Compare the provided password with the stored hashed password
+      const passwordMatch = await bcrypt.compare(body.password, user.password);
+      if (passwordMatch) {
+        const jwtResponse = await sign(
+          {
+            email: body.email,
+            id: user.id,
+          },
+          c.env.JWT_SECRET
+        );
+        return c.json({ token: jwtResponse, name: user.name });
+      }
     }
     return c.json({ msg: "The email or password provided is wrong" });
   } catch (e) {
